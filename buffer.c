de6a45a07c50b84cd5d2e5dd453aba9034c7bb7c
Janne Kulmala <janne.t.kulmala@tut.fi>
2010-07-15 01:44:42 +0300

    buffer: Fix format change getting ndetected [CORRECTIVE]

--- buffer.c
+++ buffer.c
@@ -6,22 +6,29 @@ void init_buffer(struct audio_buffer *buf)
 {
 	memset(buf, 0, sizeof(*buf));
 	buf->formatchg = (size_t) -1;
+	buf->wrap = (size_t) -1;
 }
 
 bool check_buffer_formatchg(struct audio_buffer *buf)
 {
-	if (buf->tail == buf->formatchg) {
-		buf->formatchg = (size_t) -1;
-		return true;
-	}
-	return false;
+	return (buf->tail == buf->formatchg);
 }
 
 size_t buffer_read_avail(struct audio_buffer *buf)
 {
-	if (buf->tail <= buf->head)
-		return buf->head - buf->tail;
-	return buf->wrap - buf->tail;
+	/* check for wrap around */
+	if (buf->tail >= buf->wrap) {
+		buf->tail = 0;
+		buf->wrap = (size_t) -1;
+	}
+
+	size_t end = buf->wrap;
+	if (buf->head < end && buf->head >= buf->tail)
+		end = buf->head;
+	if (buf->formatchg < end && buf->formatchg >= buf->tail)
+		end = buf->formatchg;
+
+	return end - buf->tail;
 }
 
 int buffer_write_avail(struct audio_buffer *buf, size_t min_avail)
@@ -29,18 +36,17 @@ int buffer_write_avail(struct audio_buffer *buf, size_t min_avail)
 	if (buf->head < buf->tail)
 		return buf->tail - buf->head - 1;
 
-	size_t max = BUFFER_LEN - buf->head;
-	if (max < min_avail && buf->tail != 0) {
+	size_t avail = BUFFER_LEN - buf->head;
+	if (avail < min_avail && buf->tail != 0) {
 		/* buffer wraps around */
-		if (buf->head == buf->tail) {
-			max = BUFFER_LEN;
-			buf->tail = 0;
-		} else
-			max = buf->tail - 1;
+		if (buf->head == buf->tail)
+			avail = BUFFER_LEN;
+		else
+			avail = buf->tail - 1;
 		buf->wrap = buf->head;
 		buf->head = 0;
 	}
-	return max;
+	return avail;
 }
 
 sample_t *read_buffer(struct audio_buffer *buf)
@@ -55,10 +61,9 @@ sample_t *write_buffer(struct audio_buffer *buf)
 
 void buffer_processed(struct audio_buffer *buf, size_t len)
 {
+	if (buf->tail == buf->formatchg)
+		buf->formatchg = (size_t) -1;
 	buf->tail += len;
-	/* check for wrap around */
-	if (buf->head < buf->tail && buf->tail >= buf->wrap)
-		buf->tail = 0;
 }
 
 void buffer_written(struct audio_buffer *buf, size_t len)
